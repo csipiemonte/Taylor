adb97472e2f8d7d073d15acf1e1284e33a1d668b
Marco Lucchesi <marco.lucchesi@consoft.it>
2020-10-29 11:34:41 +0100

    remedy ticket creation endpoint

--- config/routes/remedy.rb
+++ config/routes/remedy.rb
@@ -1,6 +1,7 @@
 Zammad::Application.routes.draw do
     api_path = Rails.configuration.api_path
 
-  match api_path + '/remedy_tickets',                                       to: 'remedy#index',             via: :get
+  match api_path + '/remedy_tickets',                                       to: 'remedy#index',            via: :get
   match api_path + '/sync_remedy_tickets',                                  to: 'remedy#sync',             via: :get
+  match api_path + '/create_remedy_tickets',                                to: 'remedy#create',           via: :post
 end
