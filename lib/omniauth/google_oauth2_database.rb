eb1bb7c2645c9d2b3aad5197f7dfd98bd3579614
Marco Lucchesi <marco.lucchesi@consoft.it>
2021-02-22 14:48:25 +0100

    proxy reading moved from initializer to google lib

--- lib/omniauth/google_oauth2_database.rb
+++ lib/omniauth/google_oauth2_database.rb
@@ -5,8 +5,10 @@ class GoogleOauth2Database < OmniAuth::Strategies::GoogleOauth2
 
     # database lookup
     config  = Setting.get('auth_google_oauth2_credentials') || {}
+    proxy = Setting.get('proxy') ? Setting.get('proxy') : ''
     args[0] = config['client_id']
     args[1] = config['client_secret']
+    args[2] = {client_options: {connection_opts: {proxy: proxy}}}
     super
   end
 
