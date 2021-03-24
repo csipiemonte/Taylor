e4081d0bbe856ffb2c09a016aac05d896758c22a
Marco Lucchesi <marco.lucchesi@consoft.it>
2021-03-24 17:13:18 +0100

    add some Remedy seeds

--- db/seeds/remedy.rb
+++ db/seeds/remedy.rb
@@ -32,3 +32,84 @@ Setting.create_if_not_exists(
   frontend:    false
 )
 Ticket::Article::Type.create_if_not_exists(id: 13, name: 'remedy', communication: true)
+Setting.create_if_not_exists(
+  title:       'Remedy Integration',
+  name:        'remedy_integration',
+  area:        'Integration::Switch',
+  description: 'Defines if Remedy integration is enabled or not.',
+  options:     {
+    form: [
+      {
+        display: '',
+        null:    true,
+        name:    'remedy_integration',
+        tag:     'boolean',
+        options: {
+          true  => 'yes',
+          false => 'no',
+        },
+      },
+    ],
+  },
+  state:       true,
+  preferences: {
+    prio:           1,
+    trigger:        ['menu:render', 'cti:reload'],
+    authentication: true,
+    permission:     ['admin.integration'],
+  },
+  frontend:    true
+)
+Setting.create_if_not_exists(
+  title:       'Remedy Config',
+  name:        'remedy_config',
+  area:        'Integration::Remedy',
+  description: 'Defines the Remedy Integration configurations.',
+  options:     {},
+  state:       { 'outbound' => { 'routing_table' => [], 'default_caller_id' => '' }, 'inbound' => { 'block_caller_ids' => [] } },
+  preferences: {
+    prio:       2,
+    permission: ['admin.integration'],
+  },
+  frontend:    false,
+)
+Setting.create_if_not_exists(
+  title:       'Remedy Token',
+  name:        'remedy_token',
+  area:        'Integration::Remedy',
+  description: 'Token for Remedy.',
+  options:     {
+    form: [
+      {
+        display: '',
+        null:    false,
+        name:    'remedy_token',
+        tag:     'input',
+      },
+    ],
+  },
+  preferences: {
+    permission: ['admin.integration'],
+  },
+  frontend:    false
+)
+Setting.create_if_not_exists(
+  title:       'Remedy Endpoint',
+  name:        'remedy_endpoint',
+  area:        'Integration::Remedy',
+  description: 'Endpoint for Remedy.',
+  options:     {
+    form: [
+      {
+        display: '',
+        null:    false,
+        name:    'remedy_endpoint',
+        tag:     'input',
+      },
+    ],
+  },
+  preferences: {
+    permission: ['admin.integration'],
+  },
+  frontend:    false
+)
