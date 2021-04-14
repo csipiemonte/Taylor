93dcb860176fb6836ea4d45f65c7826269c9ba20
Marco Lucchesi <marco.lucchesi@consoft.it>
2021-04-14 11:24:01 +0200

    try-catch logic when classifying ticket

--- app/models/channel/filter/classification_filter.rb
+++ app/models/channel/filter/classification_filter.rb
@@ -2,29 +2,32 @@ module Channel::Filter::ClassificationFilter
 
   def self.run(_channel, mail, ticket = nil, _article = nil, _session_user = nil)
     if Setting.get("classification_engine_enabled") == true
-      if !ticket
-        body = mail[:body].html2text
-        Rails.logger.info "CLASSIFICATION FILTER PRE - body: #{body}"
-        subject = mail[:subject].html2text
-        Rails.logger.info "CLASSIFICATION FILTER PRE - subject: #{subject}"
-        text = subject+" "+subject+" "+body
-        response = ClassificationEngineService.classify(text,0.8)
-        Rails.logger.info "CLASSIFICATION FILTER PRE - response: #{response}"
-        if response
-          mail["classification"] = response["classification"] && response["classification"]!="NORESPONSE" ? response["classification"] : response["proposed_classification"] ? response["proposed_classification"] : nil
+      begin
+        if !ticket
+          body = mail[:body].html2text
+          Rails.logger.info "CLASSIFICATION FILTER PRE - body: #{body}"
+          subject = mail[:subject].html2text
+          Rails.logger.info "CLASSIFICATION FILTER PRE - subject: #{subject}"
+          text = subject+" "+subject+" "+body
+          response = ClassificationEngineService.classify(text,0.8)
+          Rails.logger.info "CLASSIFICATION FILTER PRE - response: #{response}"
+          if response
+            mail["classification"] = response["classification"] && response["classification"]!="NORESPONSE" ? response["classification"] : response["proposed_classification"] ? response["proposed_classification"] : nil
+          end
+        else
+          Rails.logger.info "CLASSIFICATION FILTER POST - ticket: #{ticket}"
+          tag = mail["classification"]
+          Rails.logger.info "CLASSIFICATION FILTER POST - tag: #{tag}"
+          if tag
+            ticket.tag_add(tag,1)
+            ticket.save!
+          end
         end
-        true
-      else
-        Rails.logger.info "CLASSIFICATION FILTER POST - ticket: #{ticket}"
-        tag = mail["classification"]
-        Rails.logger.info "CLASSIFICATION FILTER POST - tag: #{tag}"
-        if tag
-          ticket.tag_add(tag,1)
-          ticket.save!
-        end
+      rescue e =>
+        Rails.logger.info "ERROR WHILE TRYING TO CLASSIFY TICKET WITH ID #{ticket.id}"
+      ensure
         true
       end
-      true
     end
     true
   end
