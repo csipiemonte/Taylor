8b8353a944d7b20f6da5c66c1bc1f7d4c6fed63c
marco8412 <marco.pecoraro84@gmail.com>
2021-04-19 13:54:07 +0000

    nextcrm api - 0.14, added utente riconoscuto

--- db/seeds/object_manager_attributes_csi.rb
+++ db/seeds/object_manager_attributes_csi.rb
@@ -33,3 +33,56 @@ ObjectManager::Attribute.add(
   position:    1820,
 )
 
+
+
+ObjectManager::Attribute.add(
+  force:       true,
+  object:      'Ticket',
+  name:        'utente_riconosciuto',
+  display:     'Utente Riconosciuto',
+  data_type:   'select',
+  data_option: {
+    default:    '',
+    options:    {
+      'no'           => 'No',
+      'si'            => 'Si',
+    },
+    nulloption: true,
+    multiple:   false,
+    null:       true,
+    # translate:  true,
+  },
+  editable:    true,
+  active:      true,
+  screens: { 
+    'create_middle' => { 
+      'ticket.customer' => { 
+        'shown' => true, 
+        'required' => true, 
+        'item_class' => 'column' 
+      },
+      'ticket.agent'    => { 
+        'shown' => true, 
+        'required' => true, 
+        'item_class' => 'column' 
+      } 
+    },
+    'edit' => { 
+      'ticket.customer' => { 
+        'shown' => true, 
+        'required' => true 
+      },
+      'ticket.agent' => { 
+        'shown' => true, 
+        'required' => true 
+      } 
+    } 
+  },
+
+  to_create:   false,
+  to_migrate:  false,
+  to_delete:   false,
+  position:    1830,
+)
+
+
