30a25c780b2da5487a82acaf2d98edbab797892a
Marco Lucchesi <marco.lucchesi@consoft.it>
2021-05-12 12:24:56 +0200

    fixed tables

--- db/migrate/20210511162632_create_external_activity.rb
+++ db/migrate/20210511162632_create_external_activity.rb
@@ -1,13 +1,13 @@
 class CreateExternalActivity < ActiveRecord::Migration[5.2]
   def change
-    create_table :external_activity do |t|
-      t.integer  :external_ticketing_system_id
-      t.integer  :ticket_id
-      t.string  :data
-      t.string  :bidirectional_alignment
+    create_table :external_activities do |t|
+      t.integer  :external_ticketing_system_id, null:false
+      t.integer  :ticket_id, null:false
+      t.text  :data
+      t.boolean  :bidirectional_alignment, default:true
       t.timestamps
     end
-    add_foreign_key :external_activity, :external_ticketing_system, column: :external_ticketing_system_id
-    add_foreign_key :external_activity, :tickets, column: :ticket_id
+    add_foreign_key :external_activities, :external_ticketing_systems, column: :external_ticketing_system_id
+    add_foreign_key :external_activities, :tickets, column: :ticket_id
   end
 end
