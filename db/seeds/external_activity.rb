f77e247af7f573a22b89f70d22fe6c31f8f22101
PECORARO Marco <marco.pecoraro@consulenti.csi.it>
2021-06-01 08:18:04 +0000

    merge

--- db/seeds/external_activity.rb
+++ db/seeds/external_activity.rb
@@ -74,11 +74,7 @@ remedy_model = {
       'name':'richiedente',
       'label':'richiedente',
       'type':'text',
-<<<<<<< HEAD
-      'default':'PPL000000429692',
-=======
       'default':'PPL000000018476',
->>>>>>> feature/external-activity-trigger
       'read_only': true,
       'visible': false
   },
@@ -154,14 +150,6 @@ create_translation('it-it','New external activity on','Nuova attività esterna s
 create_translation('it-it','crm operator','operatore crm')
 create_translation('it-it','external operator','operatore esterno')
 
-<<<<<<< HEAD
-create_translation('it-it','Update on an external activity related to Ticket |%s|','C\'è un aggiornamento su un\'attività esterna legata al Ticket |%s|')
-create_translation('it-it','⚠ There are new changes on this activity','⚠ Ci sono aggiornamenti su questa attività')
-create_translation('it-it','Needs Attention','Richiede Attenzione')
-
-
-=======
->>>>>>> feature/external-activity-trigger
 
 Setting.create_if_not_exists(
   title:       'External Activity Public Visibility',
