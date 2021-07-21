9154d0cb59d73292768c45a4e8a5dba96de7edc9
Marco Lucchesi <marco.lucchesi@consoft.it>
2021-07-21 16:02:15 +0200

    virtual agents types

--- app/controllers/api/nextcrm/v1/ticket_articles_controller.rb
+++ app/controllers/api/nextcrm/v1/ticket_articles_controller.rb
@@ -43,7 +43,7 @@ class Api::Nextcrm::V1::TicketArticlesController < ::TicketArticlesController
 
     # array of articles, response of index_by_ticket
     if obj_resp.is_a? Array
-      # override response array of articles with a selection of internal only articles 
+      # override response array of articles with a selection of internal only articles
       obj_resp = obj_resp.select { |article| article['internal'] == false }
       obj_resp.each do |article|
         hideInternalArticleAttributes(article)
@@ -61,30 +61,30 @@ class Api::Nextcrm::V1::TicketArticlesController < ::TicketArticlesController
 
   def hideInternalArticleAttributes(articleObj)
     whitelist_parameters = %w[
-      id 
-      ticket_id 
+      id
+      ticket_id
       type_id
-      from 
+      from
       to
-      cc 
-      subject 
-      reply_to 
-      message_id 
-      message_id_md5 
-      in_reply_to 
-      content_type  
-      references 
-      body 
+      cc
+      subject
+      reply_to
+      message_id
+      message_id_md5
+      in_reply_to
+      content_type
+      references
+      body
       attachments
-      created_at 
-      updated_at 
+      created_at
+      updated_at
     ].to_set
 
 
     if articleObj["from"] && !articleObj["from"].empty?
       article_creator = User.where(email: articleObj["created_by"]).take
-      if !article_creator || (!article_creator.permissions?("virtual_agent") && article_creator.role?('Agent'))
-        # se il commento e' creato da un Virtual Agent allora è creato per conto di un customer via api, 
+      if !article_creator || (!article_creator.permissions?("virtual_agent.api_user") && article_creator.role?('Agent'))
+        # se il commento e' creato da un Virtual Agent allora è creato per conto di un customer via api,
         # se e' creato da un Agent allora si tratta di un operatore rale e bisogna nasconderne l'identita
         articleObj["from"] = "Operatore"
       end
@@ -93,7 +93,7 @@ class Api::Nextcrm::V1::TicketArticlesController < ::TicketArticlesController
     articleObj.keys.each do |attribute|
       articleObj.delete(attribute) unless whitelist_parameters.include?(attribute)
     end
-    
+
 
   end
 
