9efc02a8ae584d2f319f2064fa2d1e26d6718d71
Carlo Peraudo <carlo.peraudo@consulenti.csi.it>
2021-10-12 15:20:23 +0200

    aligner#53

--- app/controllers/external_activity_controller.rb
+++ app/controllers/external_activity_controller.rb
@@ -6,6 +6,7 @@ class ExternalActivityController < ApplicationController
 
   def index_external_activity
     return if !params[:ticketing_system_id]
+
     system = ExternalTicketingSystem.find_by(id: params[:ticketing_system_id])
     external_activities = ExternalActivity.where(external_ticketing_system_id: params[:ticketing_system_id])
     external_activities = external_activities.where(ticket_id: params[:ticket_id]) if params[:ticket_id].present? && params[:ticket_id] != ''
@@ -14,6 +15,19 @@ class ExternalActivityController < ApplicationController
     external_activities.each do |external_activity|
       external_activity.data = process_attachments system.model, external_activity.data, external_activity, true
     end
+    render json: external_activities
+  end
+
+  # Metodo identico a 'index_external_activity' con la differenza che la query restituisce solo gli id delle external activities e non tutti i dati
+  def index_external_activity_ids
+    return if !params[:ticketing_system_id]
+
+    external_activities = ExternalActivity.where(external_ticketing_system_id: params[:ticketing_system_id])
+    external_activities = external_activities.where(ticket_id: params[:ticket_id]) if params[:ticket_id].present? && params[:ticket_id] != ''
+    external_activities = external_activities.where(archived: params[:archived]) if params[:archived].present? && params[:archived] != ''
+    external_activities = external_activities.where(delivered: params[:delivered]) if params[:delivered].present? && params[:delivered] != ''
+    external_activities = external_activities.pluck(:id)
+
     render json: external_activities
   end
 
@@ -28,9 +42,8 @@ class ExternalActivityController < ApplicationController
     return if !params[:ticketing_system_id]
     return if !params[:ticket_id]
 
-
-    system = ExternalTicketingSystem.find_by(id:params[:ticketing_system_id])
-    data = process_attachments system.model, params.permit!.to_h["data"], nil
+    system = ExternalTicketingSystem.find_by(id: params[:ticketing_system_id])
+    data = process_attachments system.model, params.permit!.to_h['data'], nil
 
     external_activity = ExternalActivity.create(
       external_ticketing_system_id: params[:ticketing_system_id],
@@ -49,14 +62,14 @@ class ExternalActivityController < ApplicationController
     external_activity = ExternalActivity.find_by(id: params[:id])
     return if !external_activity
 
-    #checking update grants for current user
+    # checking update grants for current user
     can_update = false
-    system = ExternalTicketingSystem.find_by(id:external_activity.external_ticketing_system_id)
-    access = Setting.find_by(name:"external_activity_group_access").state_current[:value]
+    system = ExternalTicketingSystem.find_by(id: external_activity.external_ticketing_system_id)
+    access = Setting.find_by(name: 'external_activity_group_access').state_current[:value]
     current_user.groups.each do |group|
       if access[system.name]
-        permission = access[system.name]["group_"+group.id.to_s]
-        if permission == "rw"
+        permission = access[system.name]['group_' + group.id.to_s]
+        if permission == 'rw'
           can_update = true
           break
         end
@@ -64,11 +77,10 @@ class ExternalActivityController < ApplicationController
     end
     return if !can_update
 
-
     new_values = params[:data]
     if new_values
       system.model.each do |_index, field|
-        if field['notify_changes'] && !new_values[field["name"]].eql?(external_activity.data[field["name"]])
+        if field['notify_changes'] && !new_values[field['name']].eql?(external_activity.data[field['name']])
           external_activity.needs_attention = true
           Role.where(name: 'Agent').first.users.where(active: true).each do |agent|
             OnlineNotification.add(
@@ -83,7 +95,7 @@ class ExternalActivityController < ApplicationController
           break
         end
       end
-      data = process_attachments system.model, params.permit!.to_h["data"], external_activity
+      data = process_attachments system.model, params.permit!.to_h['data'], external_activity
     end
 
     external_activity.data = data if data && external_activity.data != data
@@ -95,56 +107,10 @@ class ExternalActivityController < ApplicationController
     render json: external_activity
   end
 
-  def process_attachments (model, data, external_activity, decode = false)
-    model.each do |index, field|
-      next if !field['type'].eql? 'comment'
-      next if !data[field['name']]
-      comment_index = 0
-      data[field['name']].each do |comment|
-        next if !comment['attachments']
-        comment['attachments'].each do |index, attachment|
-          if decode
-            Rails.logger.debug{ "read attachment file" }
-            # non e'necessario decodificare il file prima di inviarlo al browser, viene gestito dal browser in base64 
-            # per il browser per effettuare il download del file, basta mettere il tutto in ,<a href="<metadata>,<base64_file>">
-            # ad esempio: "data:application/pdf;base64,JVBERi0xLjQKJdPr6eEKM..."
-          else
-            if attachment['to_encode']
-              Rails.logger.debug{ "write FILE" }
-              if attachment["file"].include?(';base64,')
-                # dal browser il file arriva già codificato da FileReader.readAsDataURL() in formato <metadata>,<base64(file)>
-                attachment["file"] = attachment["file"].split(';base64,')[1]
-              end
-              Rails.logger.debug{ attachment["file"] }
-              attachment.delete(:to_encode)
-            elsif external_activity
-              #attachment["file"] = external_activity.data[field["name"]][comment_index]["attachments"][index.to_s]["file"]
-            end
-          end
-        end
-        comment_index = comment_index+1
-      end
-    end
-    data
-  end
-
-  def stop_monitoring? (external_activity)
-    stop_monitoring = false
-    system = ExternalTicketingSystem.find_by(id: external_activity.external_ticketing_system_id)
-    system.model.each do |_index, field|
-      next if !field['stop_monitoring']
-
-      if field['stop_monitoring'].include?(external_activity.data[field['name']])
-        stop_monitoring = true
-      end
-    end
-    stop_monitoring
-  end
-
   def index_external_ticketing_system
     systems = ExternalTicketingSystem.all
     systems = systems.where(name: params[:name]) if params[:name].present? && params[:name] != ''
-    access = Setting.find_by(name:"external_activity_group_access").state_current[:value]
+    access = Setting.find_by(name: 'external_activity_group_access').state_current[:value]
     systems_with_permissions = []
     groups = current_user.groups
     current_user.roles.each do |role|
@@ -154,13 +120,13 @@ class ExternalActivityController < ApplicationController
 
       groups.each do |group|
         if access[system.name]
-          permission = access[system.name]["group_"+group.id.to_s]
-          if permission == "r" || permission == "rw"
+          permission = access[system.name]['group_' + group.id.to_s]
+          if permission == 'r' || permission == 'rw'
             json_system = system.as_json
             json_system[:permission] = permission
             if params[:ticket]
               activities = ExternalActivity.where(ticket_id: params[:ticket], external_ticketing_system_id: system.id)
-              json_system[:activities_needing_attention] = activities.select {|activity| activity.needs_attention}.length
+              json_system[:activities_needing_attention] = activities.select { |activity| activity.needs_attention }.length
             end
             systems_with_permissions << json_system
             break
@@ -189,12 +155,62 @@ class ExternalActivityController < ApplicationController
     system = ExternalTicketingSystem.find_by(id: params[:id])
     system_name = system.name.parameterize.underscore.downcase
     render json: {
-      integration_enabled: Setting.get(system_name+'_integration'),
-      state_alignment: Setting.get(system_name+'_state_alignment'),
-      coordinates: {
-        base_url: Setting.get(system_name+'_base_url'),
-        token: Setting.get(system_name+'_token')
+      integration_enabled: Setting.get(system_name + '_integration'),
+      state_alignment:     Setting.get(system_name + '_state_alignment'),
+      coordinates:         {
+        base_url: Setting.get(system_name + '_base_url'),
+        token:    Setting.get(system_name + '_token')
       }
     }
+  end
+
+  private
+
+  def process_attachments (model, data, external_activity, decode = false)
+    model.each do |_idx, field|
+      next if !field['type'].eql? 'comment'
+      next if !data[field['name']]
+
+      comment_index = 0
+      data[field['name']].each do |comment|
+        next if !comment['attachments']
+
+        comment['attachments'].each do |_index, attachment|
+          if decode
+            Rails.logger.debug { 'read attachment file' }
+            # non e'necessario decodificare il file prima di inviarlo al browser, viene gestito dal browser in base64
+            # per il browser per effettuare il download del file, basta mettere il tutto in ,<a href="<metadata>,<base64_file>">
+            # ad esempio: "data:application/pdf;base64,JVBERi0xLjQKJdPr6eEKM..."
+          else
+            if attachment['to_encode']
+              Rails.logger.debug { 'write FILE' }
+              if attachment['file'].include?(';base64,')
+                # dal browser il file arriva gia' codificato da FileReader.readAsDataURL() in formato <metadata>,<base64(file)>
+                attachment['file'] = attachment['file'].split(';base64,')[1]
+              end
+              Rails.logger.debug { attachment['file'] }
+              attachment.delete(:to_encode)
+            elsif external_activity
+              #attachment['file'] = external_activity.data[field['name']][comment_index]['attachments'][index.to_s]['file']
+            end
+          end
+        end
+        comment_index = comment_index + 1
+      end
+    end
+    data
+  end
+
+  def stop_monitoring? (external_activity)
+    stop_monitoring = false
+    system = ExternalTicketingSystem.find_by(id: external_activity.external_ticketing_system_id)
+    system.model.each do |_index, field|
+      next if !field['stop_monitoring']
+
+      if field['stop_monitoring'].include?(external_activity.data[field['name']])
+        stop_monitoring = true
+      end
+    end
+    stop_monitoring
   end
 end
--- config/routes/external_activity.rb
+++ config/routes/external_activity.rb
@@ -2,6 +2,7 @@ Zammad::Application.routes.draw do
   api_path = Rails.configuration.api_path
 
   match api_path + '/external_activity/system/:ticketing_system_id',      to: 'external_activity#index_external_activity',             via: :get
+  match api_path + '/external_activity_ids/system/:ticketing_system_id',  to: 'external_activity#index_external_activity_ids',         via: :get
   match api_path + '/external_activity/id/:id',                           to: 'external_activity#show_external_activity',              via: :get
   match api_path + '/external_activity',                                  to: 'external_activity#create_external_activity',            via: :post
   match api_path + '/external_activity/:id',                              to: 'external_activity#update_external_activity',            via: :put
