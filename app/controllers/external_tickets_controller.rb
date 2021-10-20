842dea2cf7047536d0c5d4f5684f3039fc90c07d
Carlo Peraudo <carlo.peraudo@consulenti.csi.it>
2021-10-20 12:35:30 +0200

    fix #145

--- app/controllers/external_tickets_controller.rb
+++ app/controllers/external_tickets_controller.rb
@@ -1,13 +1,21 @@
-# Copyright (C) 2012-2016 Zammad Foundation, http://zammad-foundation.org/
+# Copyright (C) 2021 CSI Piemonte, https://www.csipiemonte.it/
 
+# Controller custom CSI Piemonte che estende il controller VirtualAgentTicketsController che, a sua volta,
+# estende il controller TicketsController
+# Le invocazioni delle API con "/api/v1/public/tickets" sono redirette su questo controller
+# invece che sul controller nativo Zammad
+# Lo scopo di questo controller e' quello di nascondere gli stati che non devono essere visibili al chiamante.
 class ExternalTicketsController < VirtualAgentTicketsController
 
   # GET /api/v1/tickets
   def index
     super
-    @states_to_hide_array = Ticket::State.select(:id,:name,:external_state_id).where.not(external_state_id: nil).where(active: true).to_a
+
+    # external_state_id indica gli stati visibili all'esterno
+    @states_to_hide_array = Ticket::State.select(:id, :name, :external_state_id).where.not(external_state_id: nil).where(active: true).to_a
+
     # convert to hash {id => object} to improve access lookup speed in loop
-    @states_to_hide_hash = @states_to_hide_array.to_h{|state| [state.id, state] }
+    @states_to_hide_hash = @states_to_hide_array.to_h { |state| [state.id, state] }
     @states_to_hide_ids = @states_to_hide_hash.keys
     # convert array in Set to improve 'include?' lookup speed in loop
     @states_to_hide_ids = @states_to_hide_ids.to_s
@@ -29,9 +37,9 @@ class ExternalTicketsController < VirtualAgentTicketsController
   def show
     super
     ticket = Oj.load(response.body)
-    public_state = Ticket::State.find_by(id: ticket["state_id"]).external_state_id
+    public_state = Ticket::State.find_by(id: ticket['state_id']).external_state_id
     if public_state
-      ticket["state_id"] = public_state
+      ticket['state_id'] = public_state
     end
     str_resp = Oj.dump(ticket)
     response.body = str_resp
@@ -39,13 +47,15 @@ class ExternalTicketsController < VirtualAgentTicketsController
 
   private
 
+  # Metodo privato per nascondere alcuni stati
+  # Se lo stato del ticket (state_id) e' presente nell'array @states_to_hide_ids,
+  # viene sostituito con il valore presente in @states_to_hide_hash (se previsto valore).
   def hide_state (ticket)
-    if @states_to_hide_ids.include?(ticket["state_id"].to_s)
-      state = @states_to_hide_hash[ticket["state_id"]]
-      if state
-        ticket["state_id"]  = @states_to_hide_hash[ticket["state_id"]].external_state_id
-      end
-    end
-  end
+    return unless @states_to_hide_ids.include?(ticket['state_id'].to_s)
 
+    state = @states_to_hide_hash[ticket['state_id']]
+    return unless state
+
+    ticket['state_id'] = @states_to_hide_hash[ticket['state_id']].external_state_id
+  end
 end
--- app/controllers/virtual_agent_tickets_controller.rb
+++ app/controllers/virtual_agent_tickets_controller.rb
@@ -1,5 +1,9 @@
-# Copyright (C) 2012-2016 Zammad Foundation, http://zammad-foundation.org/
+# Copyright (C) 2021 CSI Piemonte, https://www.csipiemonte.it/
 
+# Controller custom CSI Piemonte che estende il controller TicketsController
+# e per i metodi 'index', 'search', 'show' aggiunge i dati inerenti le external activities,
+# qualora ci siano le abilitazioni attive
+# cfr: https://gitlab.csi.it/prodotti/nextcrm/zammad/issues/145
 class VirtualAgentTicketsController < TicketsController
 
   before_action :authorize!
@@ -22,37 +26,45 @@ class VirtualAgentTicketsController < TicketsController
   private
 
   def extend_response (response)
-      visibility = Setting.find_by(name:'external_activity_public_visibility').state_current[:value]
-      # optimized json parse
-      obj_resp = Oj.load(response.body)
-      if obj_resp.is_a? Array
-        obj_resp.each do |ticket|
-          include_external_activities visibility, ticket
-        end
-      else
-        include_external_activities visibility, obj_resp
+    visibility = Setting.find_by(name: 'external_activity_public_visibility').state_current[:value]
+    # optimized json parse
+    obj_resp = Oj.load(response.body)
+    if obj_resp.is_a? Array
+      obj_resp.each do |ticket|
+        include_external_activities visibility, ticket
       end
-      str_resp = Oj.dump(obj_resp)
-      response.body = str_resp
+    else
+      include_external_activities visibility, obj_resp
+    end
+    str_resp = Oj.dump(obj_resp)
+    response.body = str_resp
   end
 
-  def include_external_activities (visibility,ticket)
-    ticket["external_activities"] = {}
+  def include_external_activities (visibility, ticket)
+    # 'visibility' e' un hash con il nome dell'external ticketing system nella chiave e una hash nel valore.
+    # La hash del valore e' composta da tante entry cosi' composte:
+    # virtual_agent_<id>: <true/false>
+    # dove 'virtual_agent_' e' una stringa fissa, <id> e' l'ID del current user, <true/false> se e' abilitato o meno
+    # a vedere i dati delle external activities.
+    ticket['external_activities'] = {}
     ExternalTicketingSystem.all.each do |system|
-      if visibility[system.name] && visibility[system.name]["virtual_agent_"+current_user.id.to_s] == true
-        activities = []
-        ExternalActivity.where(external_ticketing_system_id:system.id, ticket_id:ticket["id"]).each do |activity|
-          nested_activity = {}
-          system.model.each do |key,field|
-            Rails.logger.info "field #{field}"
-            if field["external_visibility"] == true
-              nested_activity[field["name"]] = activity.data[field["name"]]
-            end
+      next unless visibility[system.name]
+      next if visibility[system.name]['virtual_agent_' + current_user.id.to_s] == false
+
+      activities = []
+      ExternalActivity.where(external_ticketing_system_id: system.id, ticket_id: ticket['id']).each do |activity|
+        nested_activity = {}
+
+        # system.model e' un hash
+        system.model.each_value do |field|
+          Rails.logger.info "field #{field}"
+          if field['external_visibility'] == true
+            nested_activity[field['name']] = activity.data[field['name']]
           end
-          activities.append nested_activity
         end
-        ticket["external_activities"][system.name] = activities
+        activities.append nested_activity
       end
+      ticket['external_activities'][system.name] = activities
     end
   end
 
