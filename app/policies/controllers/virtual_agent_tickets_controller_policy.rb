b8d52885caf3822dab7be3a02d153b520b8dcc48
Carlo Peraudo <carlo.peraudo@consulenti.csi.it>
2021-10-20 15:20:59 +0200

    fix permission

--- app/policies/controllers/virtual_agent_tickets_controller_policy.rb
+++ app/policies/controllers/virtual_agent_tickets_controller_policy.rb
@@ -1,3 +1,3 @@
 class Controllers::VirtualAgentTicketsControllerPolicy < Controllers::ApplicationControllerPolicy
-  default_permit!(['agent', 'virtual_agent', 'customer'])
+  default_permit!(['agent', 'virtual_agent', 'ticket.customer'])
 end
