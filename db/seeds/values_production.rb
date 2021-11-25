262f4185d27ba1dd3222a0e3989a8705371ab844
PERAUDO Carlo 2134 <carlo.peraudo@csi.it>
2021-11-25 16:44:28 +0100

    #390

--- db/seeds/values_production.rb
+++ db/seeds/values_production.rb
@@ -35,3 +35,7 @@ remedy_base_url.save!
 remedy_token = Setting.find_by(name: 'remedy_token')
 remedy_token.state = '71087ed6-07a0-3628-a0c5-5601aae89ae8'
 remedy_token.save!
+
+notification_sender_email = Setting.find_by(name: 'notification_sender')
+notification_sender_email.state = 'Taylor CSI <no-reply.assistenza.nextcrm@csi.it>'
+notification_sender_email.save!
