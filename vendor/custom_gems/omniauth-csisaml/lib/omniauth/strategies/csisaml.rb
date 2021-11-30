40b47ded074cc0dc6c029976609ecfe6198f69ce
PERAUDO Carlo 2134 <carlo.peraudo@csi.it>
2021-11-30 12:39:10 +0100

    #408 fixed uid_attribute

--- vendor/custom_gems/omniauth-csisaml/lib/omniauth/strategies/csisaml.rb
+++ vendor/custom_gems/omniauth-csisaml/lib/omniauth/strategies/csisaml.rb
@@ -3,6 +3,12 @@
 # - 'Shib-Identita-Matricola', restituito solo per IAMIDPCSI e IAMIDPCRP
 # - 'Shib-Identita-Nome', restituito sempre
 # - 'Shib-Identita-Cognome', restituito sempre
+#
+# :uid_attribute - Attribute that uniquely identifies the user.
+# default e' "uid_attribute: 'uid'"
+# Non e' possibile usare 'uid' perche' per IAMIDPCSI non e' univoco per ciascun utente,
+# quindi si prende 'Shib-Identita-CodiceFiscale' per dare un identificativo univoco
+# all'utente che si e' autenticato con SAML.
 class Csisaml < OmniAuth::Strategies::SAML
   # la custom strategy CSI deve chiamarsi 'saml' per funzionare
   # perche' si tratta di autenticazione SAML
@@ -18,7 +24,8 @@ class Csisaml < OmniAuth::Strategies::SAML
           nickname:   ['Shib-Identita-Matricola'],
           first_name: ['Shib-Identita-Nome'],
           last_name:  ['Shib-Identita-Cognome']
-        }
+        },
+        uid_attribute:        'Shib-Identita-CodiceFiscale'
       )
     args[0] = options
 
