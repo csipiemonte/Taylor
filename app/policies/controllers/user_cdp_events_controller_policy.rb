af63ce36b6fee845090e01ba552008883033bf1f
PECORARO Marco <marco.pecoraro@consulenti.csi.it>
2021-12-16 11:28:37 +0000

    remove cdp customer data platform code from branch dev

--- app/policies/controllers/user_cdp_events_controller_policy.rb
+++
@@ -1,5 +0,0 @@
-class Controllers::UserCdpEventsControllerPolicy < Controllers::ApplicationControllerPolicy
-  # permit! %i[import_example import_start], to: 'admin.user'
-  # permit! %i[search history create update], to: ['ticket.agent', 'admin.user']
-  default_permit!(['ticket.agent', 'admin.user'])
-end
