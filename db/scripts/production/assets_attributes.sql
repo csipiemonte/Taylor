ad00a65b83b948b95a6ac6504c7cb3e5e9857995
Mattia Fenu <mattia.fenu@consoft.it>
2022-01-07 09:57:26 +0100

    #446 asset_attributes table + data insert scripts

---
+++ db/scripts/production/assets_attributes.sql
@@ -0,0 +1,20 @@
+-- Popolamento della tabella "asset_attributes" per l'ambiente production
+
+INSERT INTO "zammad"."asset_attributes" (id, name, asset_id, value, created_at, updated_at)
+VALUES
+(1,  'CFI', 12, 'CCFI1146', NOW(), NOW()),
+(2,  'CFI', 14, 'CCFI1441', NOW(), NOW()),
+(3,  'CFI', 8,  'CCFI2544', NOW(), NOW()),
+(4,  'CFI', 11, 'CCFI2595', NOW(), NOW()),
+(5,  'CFI', 17, 'CCFI2597', NOW(), NOW()),
+(6,  'CFI', 5,  'CCFI2603', NOW(), NOW()),
+(7,  'CFI', 7,  'CCFI2947', NOW(), NOW()),
+(8,  'CFI', 9,  'CCFI2964', NOW(), NOW()),
+(9,  'CFI', 6,  'CCFI3069', NOW(), NOW()),
+(10, 'CFI', 3,  'CCFI3113', NOW(), NOW()),
+(11, 'CFI', 4,  'CCFI3204', NOW(), NOW()),
+(12, 'CFI', 2,  'CCFI3267', NOW(), NOW()),
+(13, 'CFI', 15, 'CCFI3268', NOW(), NOW()),
+(14, 'CFI', 1,  'CCFI3284', NOW(), NOW()),
+(15, 'CFI', 13, 'CCFI3324', NOW(), NOW()),
+(16, 'CFI', 16, 'CCFI3375', NOW(), NOW())
