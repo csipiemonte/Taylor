1f5b116e061021e431b9e5393c941f21f4e21681
PERAUDO Carlo 2134 <carlo.peraudo@csi.it>
2022-01-19 14:09:00 +0100

    Chg User.authenticate

--- app/controllers/api/nextcrm/v1/concerns/custom_api_logger.rb
+++ app/controllers/api/nextcrm/v1/concerns/custom_api_logger.rb
@@ -1,11 +1,15 @@
+# Copyright (C) 2020-2022 CSI Piemonte, https://www.csipiemonte.it/
+
 module Api::Nextcrm::V1::Concerns::CustomApiLogger
   extend ActiveSupport::Concern
 
   def apilogger
-    return APILogger.new({
-      request: request,
-      current_user: current_user
-    })
+    APILogger.new(
+      {
+        request:      request,
+        current_user: current_user
+      }
+    )
   end
 
   class APILogger < Logger
@@ -14,50 +18,48 @@ module Api::Nextcrm::V1::Concerns::CustomApiLogger
       @context = context
     end
 
-    def debug(progname = "", &block)
+    def debug(progname = '', &block)
       level = Logger::DEBUG
       rails_logger_call(level, progname, &block)
     end
 
-    def info(progname = "", &block)
+    def info(progname = '', &block)
       level = Logger::INFO
-      rails_logger_call(level,progname, &block)
+      rails_logger_call(level, progname, &block)
     end
 
-    def warn(progname = "", &block)
+    def warn(progname = '', &block)
       level = Logger::WARN
-      rails_logger_call(level,progname, &block)
+      rails_logger_call(level, progname, &block)
     end
 
-    def error(progname = "", &block)
+    def error(progname = '', &block)
       level = Logger::ERROR
-      rails_logger_call(level,progname, &block)
+      rails_logger_call(level, progname, &block)
     end
 
-    def fatal(progname = "", &block)
+    def fatal(progname = '', &block)
       level = Logger::FATAL
-      rails_logger_call(level,progname, &block)
+      rails_logger_call(level, progname, &block)
     end
 
     private
 
-    def rails_logger_call(level= nil, progname = "", &block)
+    def rails_logger_call(level = nil, progname = '', &block)
       if not block_given?
-        # se il messaggio  non 'e fornito un block ma in un argomento normail, la libreria di log di rails 
+        # se il messaggio non 'e fornito un block ma in un argomento normail, la libreria di log di rails
         # interpreta il primo argomento come il messaggio anziche' come progname e si aspetta di avere la variabile
         # progname nulla. in questo caso creo io il block per replicare il comportamento standard
         message = progname
         block = Proc.new { message }
-        progname = ""
+        progname = ''
       end
       request_id = @context[:request].uuid
       remote_ip = @context[:request].remote_ip
-      current_user_id = @context[:current_user] ? @context[:current_user].id : "null"
+      current_user_id = @context[:current_user] ? @context[:current_user].id : 'null'
       progname += "[API remote_ip: #{remote_ip} request_id:#{request_id} user_id:#{current_user_id}]"
       # https://github.com/ruby/logger/blob/master/lib/logger.rb#L494
       Rails.logger.add(level, nil, progname, &block)
     end
-
   end
-
 end
--- app/controllers/api/nextcrm/v1/concerns/reades_api_manager_jwt.rb
+++ app/controllers/api/nextcrm/v1/concerns/reades_api_manager_jwt.rb
@@ -1,23 +1,24 @@
+# Copyright (C) 2020-2022 CSI Piemonte, https://www.csipiemonte.it/
+
 module Api::Nextcrm::V1::Concerns::ReadesApiManagerJwt
   extend ActiveSupport::Concern
 
   included do
     # normalmente sarebbe una basic_auth, che non scatenerebbe il controllo del csfr
     # facendo set del current_user (virtual operator) l'autenticazione sembra provenire da sessione, e da errore il CSFR
-    skip_before_action  :verify_csrf_token
+    skip_before_action :verify_csrf_token
     # prepend mette in cima alla lista di azioni
     prepend_before_action :check_apiman_jwt
   end
 
-
-
+  # check_apiman_jwt viene eseguito come prima action, facendo:
+  # - la verifica sull'autenticazione dell'utente usato nell'invocazione delle API da Api Manager
+  # - la sostituzione dell'utente usato nell'invocazione delle API da Api Manager con l'utente
+  #   virtual agent ("#{application_name}@csi.it").
+  # Le successive chiamate di autenticazione e autorizzazione saranno effettuate sul virtual agent
   def check_apiman_jwt
     begin
-      log_input_parameters()
-      # check_apiman_jwt viene eseguito come prima action, facendo autenticazione api manager user
-      # e sostituzione con utente virtual operator. le successive chiamate di autenticazione e autorizzqzione
-      # saranno effettuate sul virtual operator
-
+      log_input_parameters
 
       # check http basic based authentication
       authenticate_with_http_basic do |username, password|
@@ -27,80 +28,78 @@ module Api::Nextcrm::V1::Concerns::ReadesApiManagerJwt
           raise Exceptions::NotAuthorized, 'API password access disabled!'
         end
 
-        user = User.authenticate(username, password)
-        # return authentication_check_prerequesits(user, 'basic_auth', auth_param) if user
-        unless user.permissions?("api_manager")
+        auth = Auth.new(username, password)
+        if !auth.valid?
+          raise Exceptions::NotAuthorized, 'Invalid BasicAuth credentials'
+        end
+
+        user = User.identify(username)
+        if !user.permissions?('api_manager')
           raise Exceptions::NotAuthorized, "user #{username} must have permission 'api_manager'"
         end
       end
 
-      apilogger.debug " api apimanager jwt ---->  #{request.headers["X-JWT-Assertion"]} "
+      apilogger.debug " api apimanager jwt ---->  #{request.headers['X-JWT-Assertion']} "
 
       # return if params[:debug]
 
-      apimanager_raw_jwt = request.headers["X-JWT-Assertion"]
+      apimanager_raw_jwt = request.headers['X-JWT-Assertion']
 
-      
-      # test_payload = { 
-      #   'http://wso2.org/claims/organization' => 'poc_nextCRM_team', 
-      #   'http://wso2.org/claims/role' => ['Internal/subscriber', 'Application/marco.lucchesi_poc_NextCRM_PRODUCTION', 'Internal/everyone'], 
-      #   'http://wso2.org/claims/applicationtier' => 'Unlimited', 
-      #   'http://wso2.org/claims/keytype' => 'PRODUCTION', 
-      #   'http://wso2.org/claims/version' => 'v1', 'iss' => 'wso2.org/products/am', 
-      #   'http://wso2.org/claims/applicationname' => 'poc_NextCRM', 
-      #   'http://wso2.org/claims/enduser' => 'marco.lucchesi@carbon.super', 
-      #   'http://wso2.org/claims/enduserTenantId' => '-1234', 'http://wso2.org/claims/company' => 'CSI', 
-      #   'http://wso2.org/claims/givenname' => 'Marco', 
-      #   'http://wso2.org/claims/applicationUUId' => '8f1662b1-767c-45d8-a250-5f05517eb93e', 
-      #   'http://wso2.org/claims/institution' => 'CSI', 
-      #   'http://wso2.org/claims/subscriber' => 'marco.lucchesi', 
-      #   'http://wso2.org/claims/tier' => 'Unlimited', 
-      #   'http://wso2.org/claims/emailaddress' => 'marco.lucchesi@consulenti.csi.it', 
-      #   'http://wso2.org/claims/lastname' => 'Lucchesi', 'exp' => 1_615_493_320, 
-      #   'http://wso2.org/claims/applicationid' => '74', 'http://wso2.org/claims/usertype' => 'APPLICATION', 
-      #   'http://wso2.org/claims/apicontext' => '/tecno/nextcrm/v1' 
+      # test_payload = {
+      #   'http://wso2.org/claims/organization' => 'poc_nextCRM_team',
+      #   'http://wso2.org/claims/role' => ['Internal/subscriber', 'Application/marco.lucchesi_poc_NextCRM_PRODUCTION', 'Internal/everyone'],
+      #   'http://wso2.org/claims/applicationtier' => 'Unlimited',
+      #   'http://wso2.org/claims/keytype' => 'PRODUCTION',
+      #   'http://wso2.org/claims/version' => 'v1', 'iss' => 'wso2.org/products/am',
+      #   'http://wso2.org/claims/applicationname' => 'poc_NextCRM',
+      #   'http://wso2.org/claims/enduser' => 'marco.lucchesi@carbon.super',
+      #   'http://wso2.org/claims/enduserTenantId' => '-1234', 'http://wso2.org/claims/company' => 'CSI',
+      #   'http://wso2.org/claims/givenname' => 'Marco',
+      #   'http://wso2.org/claims/applicationUUId' => '8f1662b1-767c-45d8-a250-5f05517eb93e',
+      #   'http://wso2.org/claims/institution' => 'CSI',
+      #   'http://wso2.org/claims/subscriber' => 'marco.lucchesi',
+      #   'http://wso2.org/claims/tier' => 'Unlimited',
+      #   'http://wso2.org/claims/emailaddress' => 'marco.lucchesi@consulenti.csi.it',
+      #   'http://wso2.org/claims/lastname' => 'Lucchesi', 'exp' => 1_615_493_320,
+      #   'http://wso2.org/claims/applicationid' => '74', 'http://wso2.org/claims/usertype' => 'APPLICATION',
+      #   'http://wso2.org/claims/apicontext' => '/tecno/nextcrm/v1'
       # }
       # apimanager_raw_jwt = JWT.encode test_payload, nil, 'none', { typ: 'JWT' }
       # one line minimal jwt:
       # apimanager_raw_jwt  = JWT.encode( {'http://wso2.org/claims/applicationname' => 'poc_NextCRM'}, nil, 'none', {typ:'JWT'} )
       ###
 
-
       decoded_jwt = JWT.decode(apimanager_raw_jwt, nil, false)
       jwt_meta = decoded_jwt[0]
-      application_name = jwt_meta["http://wso2.org/claims/applicationname"]
-      if application_name
-        application_name = application_name.downcase
-      else
-        raise Exceptions::NotAuthorized, "http://wso2.org/claims/applicationname in X-JWT-Assertion not found: #{apimanager_raw_jwt}"
-      end
+      application_name = jwt_meta['http://wso2.org/claims/applicationname']
+      raise Exceptions::NotAuthorized, "http://wso2.org/claims/applicationname in X-JWT-Assertion not found: #{apimanager_raw_jwt}" if !application_name
 
-      app_user = User.find_by(email: "#{application_name}@csi.it")
+      # Sostituzione dell'utente usato nell'invocazione delle API da Api Manager con l'utente virtual agent
+      app_user = User.find_by(email: "#{application_name.downcase}@csi.it")
       if app_user
-        current_user_set(app_user, "basic_auth")
-        user_device_log(app_user, "basic_auth")
+        current_user_set(app_user, 'basic_auth')
+        user_device_log(app_user, 'basic_auth')
         apilogger.info { "current user setted to #{current_user.email} " }
       else
-        apilogger.error{"user not found with X-JWT-Assertion http://wso2.org/claims/applicationname: #{application_name}"}
+        apilogger.error { "user not found with X-JWT-Assertion http://wso2.org/claims/applicationname: #{application_name}" }
         raise Exceptions::NotAuthorized, "user not found with X-JWT-Assertion: #{apimanager_raw_jwt}  -  http://wso2.org/claims/applicationname: #{application_name}"
-
       end
     rescue => e
       apilogger.error e
       raise Exceptions::NotAuthorized
-    end # try catch
-  end # check_api_man
+    end
+  end
 
   def log_input_parameters
-    apilogger.info { "called path: #{request.filtered_path}"}
-    apilogger.info { "input parameters: #{request.filtered_parameters}"}
+    apilogger.info { "called path: #{request.filtered_path}" }
+    apilogger.info { "input parameters: #{request.filtered_parameters}" }
     # attenzione, RAW_POST_DATA puo' contenere interi file
-    trunc_post_payload = truncate(request.env["RAW_POST_DATA"])
-    apilogger.debug { "raw input parameters: #{trunc_post_payload}"}
+    trunc_post_payload = truncate(request.env['RAW_POST_DATA'])
+    apilogger.debug { "raw input parameters: #{trunc_post_payload}" }
   end
 
   def truncate(input_text, length = 1000, truncate_string = '...[FILTERED]')
-    return '' unless input_text
+    return '' if !input_text
     # TODO, capire come prendere i primi n caratteri di Faraday::CompositeReadIO durante un multipart.
     # se si esegue #read si ottiene la stringa ma altera il body e remedy non lo recepisce
     return ' filtered multipart data' if input_text.is_a? Faraday::CompositeReadIO
@@ -110,5 +109,4 @@ module Api::Nextcrm::V1::Concerns::ReadesApiManagerJwt
     l = length - truncate_string.chars.length
     (text.length > length ? text[0...l] + truncate_string : text).to_s
   end
-
 end
