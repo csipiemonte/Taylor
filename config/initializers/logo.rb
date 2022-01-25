198af847585f13a3b9f802072f848db2428cac99
Mattia Fenu <mattia.fenu@consoft.it>
2022-01-25 14:39:00 +0100

    #454 - fix logo inizializer

--- config/initializers/logo.rb
+++ config/initializers/logo.rb
@@ -1,9 +1,8 @@
 # Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
 
 Rails.application.reloader.to_prepare do
-
-  next if !ActiveRecord::Base.connected?
-
+  # issue 454, vedere issue zammad 3933
+  #next if !ActiveRecord::Base.connected?
   # sync logo to fs / only if settings already exists
   next if ActiveRecord::Base.connection.tables.exclude?('settings')
   next if Setting.column_names.exclude?('state_current')
