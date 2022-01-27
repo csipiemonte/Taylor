36cf1bba836efdeb6f183f24ecaaaaf034b6b487
Mattia Fenu <mattia.fenu@consoft.it>
2022-01-27 16:03:22 +0100

    #454

--- config/initializers/logo.rb
+++ config/initializers/logo.rb
@@ -1,11 +1,15 @@
 # Copyright (C) 2012-2021 Zammad Foundation, http://zammad-foundation.org/
 
 Rails.application.reloader.to_prepare do
-  # issue 454, vedere issue zammad 3933
-  #next if !ActiveRecord::Base.connected?
-  # sync logo to fs / only if settings already exists
-  next if ActiveRecord::Base.connection.tables.exclude?('settings')
-  next if Setting.column_names.exclude?('state_current')
 
-  StaticAssets.sync
+  begin
+    # sync logo to fs / only if settings already exists
+    next if ActiveRecord::Base.connection.tables.exclude?('settings')
+
+    next if Setting.column_names.exclude?('state_current')
+
+    StaticAssets.sync
+  rescue ::ActiveRecord::NoDatabaseError
+    Rails.logger.debug("Database doesn't exist. Skipping StaticAssets.sync")
+  end
 end
