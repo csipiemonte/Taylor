eb76130970c06cedc37230e6256b03435c8d536b
BERRETTA Paolo 73340 <paolo.berretta@consulenti.csi.it>
2022-02-03 15:35:50 +0100

    aggiornamento assets db  #466

--- db/scripts/test/assets_attributes.sql
+++ db/scripts/test/assets_attributes.sql
@@ -1,4 +1,4 @@
--- Popolamento della tabella "asset_attributes" per l'ambiente di DEV
+-- Popolamento della tabella "asset_attributes" per l'ambiente di TEST
 
 INSERT INTO "zammad"."asset_attributes" (id, name, asset_id, value, created_at, updated_at)
 VALUES
@@ -17,6 +17,36 @@ VALUES
 (13, 'CFI', 33, 'CCFI3268', NOW(), NOW()),
 (14, 'CFI', 31, 'CCFI3284', NOW(), NOW()),
 (15, 'CFI', 29, 'CCFI3324', NOW(), NOW()),
-(16, 'CFI', 34, 'CCFI3375', NOW(), NOW())
-
+(16, 'CFI', 34, 'CCFI3375', NOW(), NOW()),
+(17, 'CFI', 42, 'CCFI3217', NOW(), NOW()),
+(18, 'CFI', 43, 'CCFI3221', NOW(), NOW()),
+(19, 'CFI', 44, 'CCFI3222', NOW(), NOW()),
+(20, 'CFI', 45, 'CCFI3223', NOW(), NOW()),
+(21, 'CFI', 46, 'CCFI3224', NOW(), NOW()),
+(22, 'CFI', 47, 'CCFI3225', NOW(), NOW()),
+(23, 'CFI', 48, 'CCFI3226', NOW(), NOW()), 
+(24, 'CFI', 49, 'CCFI3227', NOW(), NOW()),
+(25, 'CFI', 50, 'CCFI3235', NOW(), NOW()),
+(26, 'CFI', 51, 'CCFI3236', NOW(), NOW()),
+(27, 'CFI', 52, 'CCFI3237', NOW(), NOW()),
+(28, 'CFI', 53, 'CCFI3246', NOW(), NOW()),
+(29, 'CFI', 54, 'CCFI3247', NOW(), NOW()),
+(30, 'CFI', 55, 'CCFI3248', NOW(), NOW()),
+(31, 'CFI', 56, 'CCFI3276', NOW(), NOW()),
+(32, 'CFI', 57, 'CCFI3313', NOW(), NOW()),
+(33, 'CFI', 58, 'CCFI3429', NOW(), NOW()),
+(34, 'CFI', 59, 'CCFI3441', NOW(), NOW()),
+(35, 'CFI', 60, 'CCFI3442', NOW(), NOW()),
+(36, 'CFI', 61, 'CCFI3620', NOW(), NOW()),
+(37, 'CFI', 62, 'CCFI3621', NOW(), NOW()),
+(38, 'CFI', 63, 'CCFI3622', NOW(), NOW()),
+(39, 'CFI', 64, 'CCFI3623', NOW(), NOW()), 
+(40, 'CFI', 65, 'CCFI3624', NOW(), NOW()),
+(41, 'CFI', 66, 'CCFI3625', NOW(), NOW()),
+(42, 'CFI', 67, 'CCFI3653', NOW(), NOW()),
+(43, 'CFI', 68, 'CCFI3664', NOW(), NOW()),
+(44, 'CFI', 69, 'CCFI3665', NOW(), NOW()),
+(45, 'CFI', 70, 'CCFI795', NOW(), NOW()),
+(46, 'CFI', 71, 'CCFI3652', NOW(), NOW()),
+(47, 'CFI', 72, 'CCFI3354', NOW(), NOW())
 
