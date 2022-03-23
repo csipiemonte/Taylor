05588412668fd8258ebdea956654ddd01bd2db14
Mattia Fenu <mattia.fenu@consoft.it>
2022-03-23 14:33:11 +0100

    #508

--- db/seeds/ticket_priorities.rb
+++ db/seeds/ticket_priorities.rb
@@ -1,50 +1,6 @@
 # Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
 
-Ticket::Priority.create_if_not_exists(id: 1, name: __('1 high'), ui_icon: 'important', ui_color: 'high-priority')
-Ticket::Priority.create_if_not_exists(id: 2, name: __('2 normal'), default_create: true)
-Ticket::Priority.create_if_not_exists(id: 3, name: __('3 low'),  ui_icon: 'low-priority', ui_color: 'low-priority')
-Ticket::Priority.create_if_not_exists(id: 4, name: __('4 mild'), ui_icon: 'low-priority', ui_color: 'low-priority')
-
-######################
-#### Translations ####
-######################
-
-Translation.create_if_not_exists(
-  locale:         'it-it',
-  source:         '1 high',
-  target:         '1 alta',
-  target_initial: '1 alta',
-  format:         'string',
-  created_by_id:  '1',
-  updated_by_id:  '1',
-)
-
-Translation.create_if_not_exists(
-  locale:         'it-it',
-  source:         '2 normal',
-  target:         '2 normale',
-  target_initial: '2 normale',
-  format:         'string',
-  created_by_id:  '1',
-  updated_by_id:  '1',
-)
-
-Translation.create_if_not_exists(
-  locale:         'it-it',
-  source:         '3 low',
-  target:         '3 bassa',
-  target_initial: '3 bassa',
-  format:         'string',
-  created_by_id:  '1',
-  updated_by_id:  '1',
-)
-
-Translation.create_if_not_exists(
-  locale:         'it-it',
-  source:         '4 mild',
-  target:         '4 lieve',
-  target_initial: '4 lieve',
-  format:         'string',
-  created_by_id:  '1',
-  updated_by_id:  '1',
-)
+Ticket::Priority.create_if_not_exists(id: 1, name: __('critical'), ui_icon: 'important', ui_color: 'high-priority')
+Ticket::Priority.create_if_not_exists(id: 2, name: __('high'), ui_icon: 'important', ui_color: 'high-priority')
+Ticket::Priority.create_if_not_exists(id: 3, name: __('medium'), default_create: true)
+Ticket::Priority.create_if_not_exists(id: 4, name: __('low'), ui_icon: 'low-priority', ui_color: 'low-priority')
